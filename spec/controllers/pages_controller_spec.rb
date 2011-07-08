7c77e20be1048f672eb0deb6c3ef11ad065fbe9c
Jure Srsen <hoornet@gmail.com>
2011-07-08 09:30:10 +0200

    Added a title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,6 +16,11 @@ describe PagesController do
       response.should have_selector("title",
           :content => "RoR Tutorial sample app | Home")
     end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
